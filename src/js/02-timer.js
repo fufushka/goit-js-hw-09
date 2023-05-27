26b97daa18f135d8995e3e66a8389ee75b020e6e
fufushka <dimaukropdred@gmail.com>
2023-05-27 11:38:54 +0300

    s

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+const refs = {
+  datetime: document.getElementById(datetime - picker),
+};
+console.log(refs.datetime);
