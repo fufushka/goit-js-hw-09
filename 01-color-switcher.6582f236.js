9371fb074cf1774146eb3d7bb035a92bc400384d
fufushka <119953390+fufushka@users.noreply.github.com>
2023-05-27 08:33:29 +0000

    Deploying to gh-pages from @ fufushka/goit-js-hw-09@f177904e4ea84796cb2f88a6511399634272247f 🚀

---
+++ 01-color-switcher.6582f236.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop")};let n;function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e(),n=setInterval((()=>{e()}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.6582f236.js.map
