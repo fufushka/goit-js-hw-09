29dc9fa372b1a7a50aa67770c134432459ccb97e
fufushka <dimaukropdred@gmail.com>
2023-05-27 12:59:36 +0300

    ф

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,78 @@
 import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 const refs = {
-  datetime: document.getElementById(datetime - picker),
+  datetime: document.getElementById('datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
 };
-console.log(refs.datetime);
+const currentDate = new Date();
+refs.startBtn.disabled = true;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      refs.startBtn.disabled = true;
+    } else {
+      refs.startBtn.disabled = false;
+    }
+  },
+};
+flatpickr(refs.datetime, options);
+
+refs.startBtn.addEventListener('click', startTimer);
+function startTimer() {
+  refs.startBtn.disabled = true;
+  const intervalId = setInterval(() => {
+    const selectedDate = flatpickr.parseDate(
+      refs.datetime.value,
+      'Y-m-d h:i K'
+    );
+    const currentDate = new Date();
+
+    const remainingTime = selectedDate - currentDate;
+    const time = convertMs(remainingTime);
+
+    updateTimerContent(time);
+    if (remainingTime <= 0) {
+      clearInterval(intervalId);
+      updateTimerContent(convertMs(0));
+    }
+  }, 1000);
+}
+function updateTimerContent({ days, hours, minutes, seconds }) {
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
+//-----------------------------------------------------
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
